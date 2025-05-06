ba2b8d035a20fad7d274eac5e18cdbeb80225933
AdrianHaelisson <adrianhaelisson98@gmail.com>
2025-05-05 21:27:21 -0300

    Update 1005.js

--- BD_1/beecrowd/1005.js
+++ BD_1/beecrowd/1005.js
@@ -1,2 +1,8 @@
-const prompt = require("prompt-sync")();
-const nota1 = Number(prompt("Digite um número"));
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
+
+const A = parseFloat(lines[0]);
+const B = parseFloat(lines[1]);
+const MEDIA = (A * 3.5 + B * 7.5) / 11;
+
+console.log(`MEDIA = ${MEDIA.toFixed(5)}`);
