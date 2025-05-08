7ad4d3cf20a009054a02c63302f2cdc455877a38
AdrianHaelisson <adrianhaelisson98@gmail.com>
2025-05-07 21:17:59 -0300

    update

--- P_2/Prof. Hícaro/aula_07-05/dist/index.js
+++ P_2/Prof. Hícaro/aula_07-05/dist/index.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log('Olá mundo!');
+function calculaMedia(nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+}
+let media = calculaMedia(9, 8, 7);
+console.log(typeof media);
--- P_2/Prof. Hícaro/aula_07-05/src/index.ts
+++ P_2/Prof. Hícaro/aula_07-05/src/index.ts
@@ -1,1 +1,6 @@
-console.log('Olá mundo!')
+function calculaMedia(nota1: number, nota2: number, nota3: number): Number {
+  return (nota1 + nota2 + nota3) / 3;
+}
+
+let media = calculaMedia(9, 8, 7);
+console.log(typeof media);
