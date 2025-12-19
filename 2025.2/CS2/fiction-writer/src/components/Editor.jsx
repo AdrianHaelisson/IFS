7e956de018bfef1a48430c5f9ff0569643f821d8
Adrian Haelisson <adrianhaelisson98@gmail.com>
2025-12-19 20:37:43 -0300

    First commit

---
+++ 2025.2/CS2/fiction-writer/src/components/Editor.jsx
@@ -0,0 +1,8 @@
+import './Editor.css';
+export default function Editor() {
+  return (
+    <div className="editor">
+      Escreva uma história com um(a) protagonista
+    </div>
+  );
+}
---
+++ 2025.2/CS2/fiction-writer/src/components/Stories.jsx
@@ -0,0 +1,8 @@
+import './Stories.css';
+export default function Stories() {
+  return (
+    <div className="stories">
+      <h2>Histórias</h2>
+    </div>
+  );
+}
